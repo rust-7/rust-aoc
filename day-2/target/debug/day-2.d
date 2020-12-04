8cf684ab9163edb0e3e3e4f95343a364b7134399
rust7 <rust.91@icloud.com>
2020-12-04 12:42:10 +0100

    Day 2 : DONE

---
+++ day-2/target/debug/day-2.d
@@ -0,0 +1,1 @@
+/Users/rust7/rust-aoc/day-2/target/debug/day-2: /Users/rust7/rust-aoc/day-2/src/main.rs
---
+++ day-2/target/debug/deps/day_2.d
@@ -0,0 +1,5 @@
+/Users/rust7/rust-aoc/day-2/target/debug/deps/day_2: src/main.rs
+
+/Users/rust7/rust-aoc/day-2/target/debug/deps/day_2.d: src/main.rs
+
+src/main.rs:
